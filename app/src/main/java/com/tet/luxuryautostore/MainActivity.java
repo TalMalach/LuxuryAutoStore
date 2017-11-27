a814ebe43ead73f602b1e51664bd21028684f442
TalMalach <talmalach@gmail.com>
2017-11-27 22:05:30 +0200

    Initial commit

---
+++ app/src/main/java/com/tet/luxuryautostore/MainActivity.java
@@ -0,0 +1,55 @@
+package com.tet.luxuryautostore;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button AboutButton;
+    private Button loginButton;
+
+
+    public void aboutButtonClicked()
+    {
+    AboutButton = (Button)findViewById(R.id.main_button_about);
+        AboutButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            //finish();
+            startActivity(intent);
+        }
+    });
+    }
+
+    public void registerButtonClicked()
+    {
+        loginButton = (Button)findViewById(R.id.login_register_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                //finish();
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        aboutButtonClicked();
+        registerButtonClicked();
+
+    {
+
+
+
+    }
+    }
+}
